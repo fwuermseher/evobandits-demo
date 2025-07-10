9c128789190bb7fe65b3f4af6e8802b0c6661d42
fwuermseher <f.wuermseher@t-online.de>
2025-07-10 17:57:27 +0200

    add fixed instance for the traveling salesman problem

---
+++ genetic_algorithm/__init__.py
@@ -0,0 +1,15 @@
+from ._tsp import (
+    TSP_CITIES, 
+    TSP_DIST_MATRIX,
+    TSP_N_CITIES,
+    TSP_OPT_COST,
+    TSP_OPT_TOUR,
+)
+
+__all__ = [
+    "TSP_CITIES",
+    "TSP_DIST_MATRIX",
+    "TSP_N_CITIES",
+    "TSP_OPT_COST",
+    "TSP_OPT_TOUR",
+]
